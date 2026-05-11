54889e144b41c58add6c057ec6db19f33108905f
Eduardo <eduzzfelipe21@gmail.com>
2026-05-11 18:27:11 -0300

    style: ajustes nos estilos do header nav(pc)

--- login_system/src/pages/Dashboard.jsx
+++ login_system/src/pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import { useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { Bell } from "lucide-react";
 import { Search } from "lucide-react";
-import { ChevronDown } from 'lucide-react';
+import { ChevronDown } from "lucide-react";
 import userImg from "../assets/userImg.png";
 import { AuthContext } from "../context/authContext";
 
@@ -21,34 +21,48 @@ function Dashboard() {
     }
 
     verifyUser();
-  });
+  }, [navigate]);
 
   return (
-    <>
-      <nav className="w-screen flex justify-end">
-        <div className="bg-[#1A1C20] w-[80%] flex justify-around gap-2 text-left pl-10 h-15 items-center bg-transparent-2">
-          <span className="text-gray-500 flex-col  text-[18px] mr-2">
-            Dashboard {">"}{" "}
-            <span className="text-white text-[18px]">Geral</span>
-          </span>
-          <div className="flex items-center ">
-            <Search className="text-[#9CA3AF] w-5 left-8 relative z-1  bg-none" />
-            <input
-              type="text"
-              placeholder="Buscar"
-              className=" w-60 h-10  pl-10 relative placeholder:relative border-[#3d4044] border rounded-lg
-               bg-[#2D3035] outline-0 text-gray-400"
-            />
+    < >
+      <div className=" w-screen h-screen bg-[#2D3035] ">
+        {/* Menu PC */}
+        <nav className="w-screen hidden  lg:flex justify-end">
+          <div
+            className="bg-[#1A1C20] w-[80%] flex justify-between
+             h-18 items-center rounded-t-lg -translate-x-0.5"
+          >
+            <div className=" flex ml-4">
+              <span className="text-gray-500 flex-col  text-[18px] mr-2">
+                Dashboard {">"}{" "}
+                <span className="text-white text-[18px]">Geral</span>
+              </span>
+            </div>
+            <div className="flex items-center gap-4">
+              <div className="flex items-center relative">
+                <Search
+                  className="text-[#9CA3AF] w-5 left-8
+                              relative z-1  bg-none"
+                />
+                <input
+                  type="text"
+                  placeholder="Buscar"
+                  className=" w-60 h-10  pl-10 relative
+                   border-[#3d4044] border rounded-lg
+                   bg-[#2D3035] outline-0 text-gray-100"
+                />
+              </div>
+              <Bell className="text-gray-400" />
+              <img src={userImg} className="w-10" alt="" />
+              <div className="flex flex-col">
+                <span className="text-white text-[16px]">{nome}</span>
+                <span className="text-gray-400 text-[14px]">Logado há 2h</span>
+              </div>
+              <ChevronDown className="text-white mr-4" />
+            </div>
           </div>
-          <Bell className="text-gray-400" />
-          <img src={userImg} className="w-10" alt="" />
-          <div className="flex flex-col">
-            <span className="text-white">{nome}</span>
-            <span className="text-gray-400">logado há 2h</span>
-          </div>
-          <ChevronDown className="text-white"/>
-        </div>
-      </nav>
+        </nav>
+      </div>
     </>
   );
 }
