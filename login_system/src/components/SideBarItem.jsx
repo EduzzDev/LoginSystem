02b579d6653ffe74e8b648c05931970755a663fb
Eduardo <eduzzfelipe21@gmail.com>
2026-05-18 20:24:12 -0300

    Feat: Adicionei hover e active caso usuário clique nas seções da lista

--- login_system/src/components/SideBarItem.jsx
+++ login_system/src/components/SideBarItem.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
+
 function SideBarItem({ children }) {
+  const [isActive, setIsActive] = useState(false);
   return (
-    <li className="flex text-gray-300  text-xl items-center mb-6 ">
+    <li
+      onClick={() => setIsActive(!isActive)}
+      className={`flex w-[95%] p-2.5 rounded-2xl  hover:bg-gray-600 hover:text-white  text-xl items-center mb-2 cursor-pointer
+        ${
+          isActive
+            ? " w-[95%] p-2.5 rounded-2xl bg-gray-600 text-white border-gray-200"
+            : ""
+        }`}
+    >
       {children}
     </li>
   );
