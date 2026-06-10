7db1edbe00aa867e0a76ad48576f562c1b4b2c8f
Eduardo <eduzzfelipe21@gmail.com>
2026-06-10 18:20:03 -0300

    Feat: adicionei novas routes para MyProfile, Security, and Help pages no componente App

--- login_system/src/App.jsx
+++ login_system/src/App.jsx
@@ -4,7 +4,10 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Tasks from "./pages/Tasks";
+import MyProfile from "./pages/MyProfile";
 import { AuthProvider } from "./context/auth";
+import Security from "./pages/Security";
+import Help from "./pages/HelpPage"
 
 function App() {
   return (
@@ -16,6 +19,9 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/tasks" element={<Tasks />} />
+          <Route path="/myProfile" element={<MyProfile />} />
+          <Route path="/security" element={<Security />} />
+          <Route path="/help" element={<Help />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
