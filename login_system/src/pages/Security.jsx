e2efeb9803fa0ff32bf0fc90d75f75e80f4ab32b
Eduardo <eduzzfelipe21@gmail.com>
2026-06-10 18:21:20 -0300

    Feat: implementada a página Security com autenticação e navegação

---
+++ login_system/src/pages/Security.jsx
@@ -0,0 +1,137 @@
+import { checkAuth } from "../services/api";
+import { logout } from "../services/api";
+import SideBarItem from "../components/SideBarItem";
+import { useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { Bell } from "lucide-react";
+import { Search } from "lucide-react";
+import { ChevronDown } from "lucide-react";
+import { LayoutDashboard } from "lucide-react";
+import { User } from "lucide-react";
+import { ClipboardList } from "lucide-react";
+import { Shield } from "lucide-react";
+import { ShieldQuestionMark } from "lucide-react";
+import { LogOut } from "lucide-react";
+import { Zap } from "lucide-react";
+import userImg from "../assets/userImg.png";
+import { AuthContext } from "../context/authContext";
+
+function Security() {
+  const navigate = useNavigate();
+  const { user, timeLogged } = useContext(AuthContext);
+
+  useEffect(() => {
+    async function verifyUser() {
+      try {
+        await checkAuth();
+      } catch {
+        navigate("/");
+      }
+    }
+
+    verifyUser();
+  }, [navigate]);
+
+  async function handleLogoutClick() {
+    try {
+      await logout();
+      alert(`You logged out with success`);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  function handleDashboard() {
+    navigate("/dashboard");
+  }
+  function handleTasks() {
+    navigate("/tasks");
+  }
+  function myProfile() {
+    navigate("/myProfile");
+  }
+  function handleHelp() {
+    navigate("/help");
+  }
+  return (
+    <>
+      {/* Menu PC */}
+      <div className=" w-screen h-screen hidden lg:block bg-[#2D3035] ">
+        <nav className="w-screen hidden  lg:flex justify-end">
+          <div
+            className="bg-[#1A1C20] w-[80%] flex justify-between
+             h-18 items-center rounded-t-lg -translate-x-0.5  border-b  border-gray-700 "
+          >
+            <div className=" flex ml-4">
+              <span className="text-gray-500 flex-col  text-[18px] mr-2">
+                Dashboard {">"}{" "}
+                <span className="text-white text-[18px]">Security</span>
+              </span>
+            </div>
+            <div className="flex items-center gap-4">
+              <div className="flex items-center relative">
+                <Search
+                  className="text-[#9CA3AF] w-5 left-8
+                              relative z-1  bg-none"
+                />
+                <input
+                  type="text"
+                  placeholder="Buscar"
+                  className=" w-60 h-10  pl-10 relative
+                   border-[#3d4044] border rounded-lg
+                   bg-[#2D3035] outline-0 text-gray-100"
+                />
+              </div>
+              <Bell className="text-gray-400" />
+              <img src={userImg} className="w-10" alt="" />
+              <div className="flex flex-col">
+                <span className="text-white text-[16px]">{user}</span>
+                <span className="text-gray-400 text-[14px]">
+                  Logado há {timeLogged}{" "}
+                </span>
+              </div>
+              <ChevronDown className="text-white mr-4" />
+            </div>
+          </div>
+        </nav>
+        <nav className=" w-[20%] h-full flex flex-col justify-between pl-6 border-r border-gray-600  absolute bottom-0 text-white bg-[#1A1C20] ">
+          <h1 className="flex relative text-3xl top-4.5 items-center text-gray-200">
+            <Zap className="relative mr-1 ml-1 " />
+            <span className="font-extrabold">Login</span>System
+          </h1>
+          <ul className="flex flex-col items-baseline">
+            <SideBarItem onClick={() => handleDashboard()}>
+              <LayoutDashboard className=" mr-3 ml-2" />
+              Geral
+            </SideBarItem>
+            <SideBarItem onClick={() => myProfile()}>
+              <User className=" mr-4 ml-1" />
+              Meu Perfil
+            </SideBarItem>
+            <SideBarItem onClick={() => handleTasks()}>
+              <ClipboardList className=" mr-4 ml-1" />
+              Tarefas
+            </SideBarItem>
+            <SideBarItem>
+              <Shield className=" mr-4 ml-1" />
+              Segurança
+            </SideBarItem>
+            <SideBarItem onClick={ () => handleHelp()}>
+              <ShieldQuestionMark className=" mr-4 ml-1" />
+              Ajuda
+            </SideBarItem>
+            <SideBarItem onClick={() => handleLogoutClick()}>
+              <LogOut className=" mr-4 ml-1" />
+              Sair
+            </SideBarItem>
+          </ul>
+          <SideBarItem onClick={() => handleLogoutClick()}>
+            <LogOut className=" mr-4 ml-1" />
+            Sair
+          </SideBarItem>
+        </nav>
+      </div>
+    </>
+  );
+}
+export default Security;
