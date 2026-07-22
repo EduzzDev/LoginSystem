01dec2f82dbb24dc80c679370ea1c07a8e38b0bd
Eduardo <eduzzfelipe21@gmail.com>
2026-07-22 14:07:41 -0300

    Feat: alterei a verificação do jwt para um arquivo separado e padronizado

---
+++ backend/routes/middleware.js
@@ -0,0 +1,21 @@
+import pkg from "jsonwebtoken";
+const { verify } = pkg;
+
+function verificarAutenticacao(req, res, next) {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  try {
+    const decoded = verify(token, process.env.JWT_SECRET);
+    console.log("decoded:", decoded);
+    req.userId = decoded.userId;
+    next();
+  } catch {
+    return res.status(401).json({ error: "Invalid token" });
+  }
+}
+
+export default verificarAutenticacao;
