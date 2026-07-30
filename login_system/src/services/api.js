01c5f62738a42bfb4b11d6366fa51ef819b5d728
Eduardo <eduzzfelipe21@gmail.com>
2026-07-30 17:41:39 -0300

    Feat: Adicionando o cargo a função que atualiza o perfil do usuario

--- login_system/src/services/api.js
+++ login_system/src/services/api.js
@@ -72,14 +72,14 @@ export async function getUserProfile() {
   return result;
 }
 
-export async function updateUserProfile(name, email) {
+export async function updateUserProfile(name, email, cargo) {
   const res = await fetch(`${BASE_URL}/user/update-profile`, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
     },
     credentials: "include",
-    body: JSON.stringify({ name, email }),
+    body: JSON.stringify({ name, email, cargo }),
   });
   const result = await res.json();
   if (!res.ok) {
