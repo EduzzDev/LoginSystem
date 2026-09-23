90e41db3b6fe65f26f06db1368336f89c97bc2dc
Eduardo <eduzzfelipe21@gmail.com>
2026-09-22 21:40:33 -0300

    fix: corrigido o carregamento da página e atualizado a importação do componente ForgotPassword

--- login_system/src/App.jsx
+++ login_system/src/App.jsx
@@ -1,9 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import "./App.css";
+import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Tasks from "./pages/Tasks";
 import { AuthProvider } from "./context/auth";
-import LoadingScreen from "./components/LoadingScreen";
+import Help from "./pages/HelpPage";
+import ForgotPassword from "./pages/ForgotPassword";
 import React, { lazy, Suspense } from 'react';
 
 const DashboardPage = lazy(() => import('./pages/Dashboard'));
@@ -12,7 +15,6 @@ const MyProfilePage = lazy(() => import('./pages/MyProfile'));
 const RegisterPage = lazy(() => import("./pages/Register"));
 const TasksPage = lazy(() => import("./pages/Tasks"));
 const HelpPage = lazy(() => import("./pages/HelpPage"));
-const ForgotPasswordPage = lazy(() => import("./pages/ForgotPassword"));
 
 function App() {
   return (
@@ -20,11 +22,11 @@ function App() {
       <Toaster position="top-center" reverseOrder={false} />
       <AuthProvider>
         <BrowserRouter>
-          <Suspense fallback={<LoadingScreen />}>
+          <Suspense fallback={<div className="spinner">Carregando página...</div>}>
             <Routes>
               <Route path="/" element={<Login />} />
               <Route path="/register" element={<RegisterPage />} />
-              <Route path="/forgot" element={<ForgotPasswordPage />}></Route>
+              <Route path="/forgot" element={<ForgotPassword />}></Route>
               <Route path="/dashboard" element={<DashboardPage />} />
               <Route path="/tasks" element={<TasksPage />} />
               <Route path="/myProfile" element={<MyProfilePage />} />
